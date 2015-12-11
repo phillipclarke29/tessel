bb1a6ccd58e3018beea8f662677bed9347a69157
philip <philip.clarke@redhood.co.uk>
2015-12-11 12:32:28 +0000

    post tested with postman as stub client

--- project/server.js
+++ project/server.js
@@ -9,10 +9,19 @@ app.get('/api/items', function(req,res){
     {
       model: 'tessel1',
       body: 'Very cool item',
+      price: 19.99
     }
 
   ])
 })
+
+app.post('/api/items', function(req,res){
+  console.log('Item received');
+  console.log(req.body.model);
+  console.log(req.body.body);
+  res.send(201)
+
+});
 
 app.listen(3000, function(){
   console.log('cool - server listening on', 3000);
