e99bc75cc1388f1343307c8ad983c91a0671820d
philip <philip.clarke@redhood.co.uk>
2015-12-14 12:55:22 +0000

    now posting simple data to api - next need to fix the form

--- project/models/item.js
+++ project/models/item.js
@@ -2,7 +2,7 @@ var db = require('../db')
 var Item = db.model('Item', {
  model: { type: String, required: true },
  body:  { type: String, required: true },
- price: { type: Number, required: false }
+ price: { type: Number, required: true }
 })
 
 module.exports = Item
