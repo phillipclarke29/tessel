7acccd42beefb3c483ffeb3f64c39214080ce010
philip <philip.clarke@redhood.co.uk>
2015-12-14 12:34:43 +0000

    added items array from api to angular store.html

---
+++ app 20-52-32-082.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function(request,response){
+  response.writeHead(200, {"Content-Type":"Text/plain"});
+  response.write("Hello world");
+  response.end();
+}).listen(3000);
