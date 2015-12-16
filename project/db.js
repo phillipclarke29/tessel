a00ab51248b3a98cc80e2bea5b62b6920d007d02
philip <philip.clarke@redhood.co.uk>
2015-12-16 13:48:39 +0000

    changed project to app

--- project/db.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tessel', function(){
-  console.log('mongo connected');
-
-})
-
-module.exports = mongoose
--- project/models/item.js
+++
@@ -1,9 +0,0 @@
-var db = require('../db')
-var Item = db.model('Item', {
- model: { type: String, required: true },
- body:  { type: String, required: true },
- mid: { type: String, required: false },
- price: { type: Number, required: true }
-})
-
-module.exports = Item
