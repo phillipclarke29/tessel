1d52e39843a8ce0b83c3f93fd226a0e4fbda205f
philip <philip.clarke@redhood.co.uk>
2015-12-16 17:21:45 +0000

    added ordering to object

--- project/server.js
+++ project/server.js
@@ -11,8 +11,6 @@ app.get('/', function(req,res){
   res.sendfile('layouts/store.html')
 });
 
-
-
 app.get('/api/items', function(req,res, next){
   Item.find(function(err, items){
     if(err) {return next(err)}
