ca7ccd20c4d71d65c89a278b1623b03a55d0a1a5
philip <philip.clarke@redhood.co.uk>
2015-12-18 11:27:50 +0000

    added gulp ng-annotate

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate')
 gulp.task('scripts', function() {
-  return gulp.src('./ng/*.js')
+  return gulp.src(['./ng/module.js','./ng/*.js'])
   .pipe(concat('app.js'))
+  .pipe(ngAnnotate())
+  .pipe(uglify())
   .pipe(gulp.dest('./assets/'));
 });
