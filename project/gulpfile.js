f68ec32059b2b649dc6b65242a9b774b9340d47f
philip <philip.clarke@redhood.co.uk>
2015-12-19 13:00:44 +0000

    css now being compiled with gulp - checking watch

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -4,6 +4,7 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
+
 gulp.task('scripts', function() {
   return gulp.src(['./ng/module.js','./ng/*.js'])
   .pipe(sourcemaps.init())
@@ -13,17 +14,18 @@ gulp.task('scripts', function() {
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('./assets/'));
 });
-gulp.task('watch:scripts',['scripts'],function(){
-  gulp.watch('./ng/*.js',['scripts'])
 
-})
+gulp.task('watch:scripts', ['scripts'], function() {
+  gulp.watch('./ng/*.js', ['scripts']);
 
-gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('.assets/css/'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass', function() {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/stylesheets/'));
+});
+
+gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
