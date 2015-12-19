24e4aac115ca60bf5094f099860204783b4ab8a8
philip <philip.clarke@redhood.co.uk>
2015-12-19 12:37:46 +0000

    setting up gulp and sass

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
@@ -16,3 +17,13 @@ gulp.task('watch:scripts',['scripts'],function(){
   gulp.watch('./ng/*.js',['scripts'])
 
 })
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('.assets/css/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
